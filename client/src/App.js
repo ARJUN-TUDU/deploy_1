7d116a0aba94c24417458a94bd376fdbef4017a3
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-08-10 23:05:13 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://new-1.onrender.com:4000")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
