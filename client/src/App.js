168b90969dcbb8c89959a084cbab6e6a4bdead02
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-08-10 23:27:14 +0530

    1

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("https://new-1.onrender.com:4000")
+    fetch("https://new-1.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
